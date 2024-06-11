48e2b4d03decf523622ec296beb3354978eea104
rizwanshaikh199091 <rizwanshaikh199091@gmail.com>
2024-06-11 11:48:58 +0530

    second commit
    second commit

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -6,4 +6,8 @@ const router = express.Router();
 
 router.get('/', authMiddleware, fetchNews);
 
+router.get('/checkserver', function(req, res){
+    res.json({ message: 'Server is up and running!' });
+});
+
 module.exports = router;
